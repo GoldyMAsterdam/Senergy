11e345da24e9238b423c0516f3f68172bc5739a3
MA-Silent <38739@ma-web.nl>
2025-02-14 14:51:44 +0100

    BACKEND COMPLETE(meer en deel)

---
+++ Test/public/post-esp-data.php
@@ -0,0 +1,44 @@
+<?php
+$servername = "mariadb";
+$dbname = "Test";
+$username = "test";
+$password = "1234";
+
+$api_key= $sensor = $location = $value1 = $value2 = $value3 = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $sensor = test_input($_POST["sensor"]);
+        $location = test_input($_POST["location"]);
+        $value1 = test_input($_POST["value1"]);
+        $value2 = test_input($_POST["value2"]);
+        $value3 = test_input($_POST["value3"]);
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            exit("Connection failed: " . $conn->connect_error);
+        } 
+        
+        $sql = "INSERT INTO SensorData (sensor, location, value1, value2, value3)
+        VALUES ('" . $sensor . "', '" . $location . "', '" . $value1 . "', '" . $value2 . "', '" . $value3 . "')";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } 
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    
+        $conn->close();
+}
+else {
+    echo "No data posted with HTTP POST.";
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const menu = document.getElementById('Hamburber-Menu');
 const body = document.body;
+const nav = document.getElementById('dataSelection')
 menu.addEventListener('click', ()=>{
-    if(body.style.backgroundColor == "red"){
-        body.style.backgroundColor = "white";
-    }else{
-        body.style.backgroundColor = "red";
-    }
+    // if(body.style.backgroundColor == "red"){
+    //     body.style.backgroundColor = "white";
+    // }else{
+    //     body.style.backgroundColor = "red";
+    // }
+    visibilityToggle(nav);
 })
+
+function visibilityToggle(Element){
+    if(!(Element.style.visibility == 'visible')){
+        Element.style.visibility = 'visible';
+    }else{
+        Element.style.visibility = 'hidden';
+    }
+}
